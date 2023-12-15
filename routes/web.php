d85990186c7e35bdc2fdb6d6306aa94d29f3b6a6
larissa.silva <larissa.silva@auditto.com.br>
2023-12-15 07:19:50 -0300

    agrupando as rotas

--- routes/web.php
+++ routes/web.php
@@ -34,63 +34,73 @@ Route::middleware([
     })->name('dashboard');
 });
 
-
 Route::get('/', function () {
     return view('site.index');
 })->name('home');
 
-Route::get('/categorias', function () {
-    return view('site.categorias');
-})->name('categorias');
+Route::prefix('categorias')->group(function () {
+    
+    Route::get('/', function () {
+        return view('site.categorias');
+    })->name('categorias');
 
-Route::get('/quem-somos', function () {
-    return view('site.quem-somos');
-})->name('quem-somos');
+    Route::get('/classic-physique', function () {
+        return view('site.classic-physique');
+    })->name('classic-physique');
+});
 
-Route::get('/quem-somos/comissao', function () {
-    return view('site.comissao');
-})->name('comissao');
+Route::prefix('quem-somos')->group(function () {
+    Route::get('/', function () {
+        return view('site.quem-somos');
+    })->name('quem-somos');
+    
+    Route::get('/comissao', function () {
+        return view('site.comissao');
+    })->name('comissao');
+});
 
 Route::get('/filiacao', function () {
     return view('site.filiacao');
 })->name('filiacao');
 
-Route::get('/campeonatos', function () {
-    return view('site.campeonatos');
-})->name('campeonatos');
+Route::prefix('campeonatos')->group(function () {
+    Route::get('/', function () {
+        return view('site.campeonatos');
+    })->name('campeonatos');
+    
+    Route::get('/calendario', function () {
+        return view('site.calendario');
+    })->name('calendario');
+    
+    Route::get('/como-competir', function () {
+        return view('site.como-competir');
+    })->name('como-competir');
+    
+    Route::get('/backstage-anual', function () {
+        return view('site.backstage-anual');
+    })->name('backstage-anual');
+    
+    Route::get('/inscricao', function () {
+        return view('site.inscricao');
+    })->name('inscricao');
+    
+    Route::prefix('resultados')->group(function () {
+        Route::get('/2023', function () {
+            return view('site.resultado-2023');
+        })->name('resultado-2023');
+    });
+});
 
-Route::get('/calendario', function () {
-    return view('site.calendario');
-})->name('calendario');
+Route::prefix('educacao')->group(function () {
+    Route::get('/', function () {
+        return view('site.educacao');
+    })->name('educacao');
 
-Route::get('/como-competir', function () {
-    return view('site.como-competir');
-})->name('como-competir');
-
-Route::get('/backstage-anual', function () {
-    return view('site.backstage-anual');
-})->name('backstage-anual');
-
-Route::get('/inscricao', function () {
-    return view('site.inscricao');
-})->name('inscricao');
-
-Route::get('/resultado-2023', function () {
-    return view('site.resultado-2023');
-})->name('resultado-2023');
-
-Route::get('/educacao', function () {
-    return view('site.educacao');
-})->name('educacao');
-
-Route::get('/curso-arbitros', function () {
-    return view('site.curso-arbitros');
-})->name('curso-arbitros');
+    Route::get('/curso-arbitros', function () {
+        return view('site.curso-arbitros');
+    })->name('curso-arbitros');
+});
 
 Route::get('/contato', function () {
     return view('site.contato');
 })->name('contato');
-
-Route::get('/categoria/classic-physique', function () {
-    return view('site.classic-physique');
-})->name('classic-physique');
