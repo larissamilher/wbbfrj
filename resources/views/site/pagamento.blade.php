48271f224328daca1b5a2fbd4fbc9b95bb478721
larissa.silva <larissa.silva@auditto.com.br>
2023-12-20 19:26:07 -0300

    parcelamento

--- resources/views/site/pagamento.blade.php
+++ resources/views/site/pagamento.blade.php
@@ -30,31 +30,52 @@
                                             <h3>Resumo da Compra</h3>
                                             <h1>{{$campeonato->nome}}</h1>
                                             <h2> Valor Total da Compra </h2>
-                                            <h1>R$ {{  number_format( $campeonato->valor, 2, ',', '.')}}</h1>
+                                            <h1 id="label-valor-compra">R$ {{  number_format( $campeonato->valor, 2, ',', '.')}}</h1>
                                         </div>      
                                     </div>
                                 </div>   
                                 <div class="col-lg-6 col-md-6">
-                                    <div class="row">
+                                    <div class="row" style="text-align: left;">
                                 
                                         <div class="col-lg-12 col-md-12">
                                             <div class="form-box email-icon mb-30">
-                                                <input type="text" name="nome_cartao" id="nome_cartao" placeholder="Nome no cartão" value="{{isset($retorno['cartao']['nome']) ?$retorno['cartao']['nome'] : '' }}">
+                                                <label for="nome_cartao">Nome no cartão <span>*</span> </label>
+                                                <input type="text" name="nome_cartao" id="nome_cartao"  value="{{isset($retorno['cartao']['nome']) ?$retorno['cartao']['nome'] : '' }}">
                                             </div>
                                         </div>   
                                         <div class="col-lg-12 col-md-12">
                                             <div class="form-box email-icon mb-30">
-                                                <input type="text" name="numero_cartao" id="numero_cartao" placeholder="Número do Cartão" value="{{isset($retorno['cartao']['numero']) ?$retorno['cartao']['numero'] : '' }}">
+                                                <label for="numero_cartao">Número do Cartão <span>*</span> </label>
+                                                <input type="text" name="numero_cartao" id="numero_cartao"  value="{{isset($retorno['cartao']['numero']) ?$retorno['cartao']['numero'] : '' }}">
                                             </div>
                                         </div>  
                                         <div class="col-lg-12 col-md-12">
                                             <div class="form-box email-icon mb-30">
-                                                <input type="text" name="validade_cartao" id="validade_cartao" placeholder="Data de validade" value="{{isset($retorno['cartao']['validade']) ?$retorno['cartao']['validade'] : '' }}">
+                                                <label for="validade_cartao">Data de validade <span>*</span> </label>
+                                                <input type="text" name="validade_cartao" id="validade_cartao" value="{{isset($retorno['cartao']['validade']) ?$retorno['cartao']['validade'] : '' }}">
                                             </div>
                                         </div>  
                                         <div class="col-lg-12 col-md-12">
                                             <div class="form-box email-icon mb-30">
-                                                <input type="text" name="cvv" id="cvv" placeholder="Código de segurança" value="{{isset($retorno['cartao']['ccv']) ?$retorno['cartao']['ccv'] : '' }}">
+                                                <label for="nome_cartao">Código de segurança <span>*</span> </label>
+                                                <input type="text" name="cvv" id="cvv"value="{{isset($retorno['cartao']['ccv']) ?$retorno['cartao']['ccv'] : '' }}">
+                                            </div>
+                                        </div>    
+                                        <div class="col-lg-12 col-md-12" style=" margin-bottom: 3%;">
+                                            <div class="form-box email-icon mb-30">
+                                                <label for="parcelamento">Plano de parcelamento  <span>*</span> </label>
+                                                <select name="parcelamento" id="parcelamento">
+                                                    <option value="1" selected data-valor="{{  number_format( $campeonato->valor, 2, ',', '.')}}"> 1 x R${{  number_format( $campeonato->valor, 2, ',', '.')}} *sem juros</option>
+                                                    <option value="2" data-valor="
+                                                    2 * {{ number_format((($campeonato->valor / 2) * (1 + 0.0249 * 2) + 0.49), 2, '.', '.') }}
+                                                    "> 2 x R${{ number_format((($campeonato->valor / 2) * (1 + 0.0249 * 2) + 0.49), 2, ',', '.') }} *com juros</option>
+                                                    <option value="3" data-valor="
+                                                    3 * {{ number_format(($campeonato->valor / 3) * (1 + 0.0249 * 3) + 0.49, 2, '.', '.') }}
+                                                    "> 3 x R${{ number_format(($campeonato->valor / 3) * (1 + 0.0249 * 3) + 0.49, 2, ',', '.') }} *com juros</option>
+                                                    <option value="4" data-valor="
+                                                    4 * {{ number_format(($campeonato->valor / 4) * (1 + 0.0249 * 4) + 0.49, 2, '.', '.') }} 
+                                                    "> 4 x R${{ number_format(($campeonato->valor / 4) * (1 + 0.0249 * 4) + 0.49, 2, ',', '.') }} *com juros</option>
+                                                </select>
                                             </div>
                                         </div>    
                                     </div>
@@ -102,7 +123,16 @@
 
         $(document).ready(function() {
 
-        
+            $('#parcelamento').on('change', function() {
+
+                var dataValor = $('#parcelamento option:selected').data('valor');
+                dataValor = dataValor.replace(/,/g, '.');
+                var resultado = eval(dataValor);
+                resultado = resultado.toFixed(2).replace('.', ',');
+
+                $("#label-valor-compra").html('R$ ' + resultado);
+                
+            });        
         });
 
     </script>
