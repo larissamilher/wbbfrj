8f5539ebdc56ba4ce3b6af399a06eb8bafecbd8c
larissa.silva <larissa.silva@auditto.com.br>
2023-12-23 16:45:36 -0300

    fix

--- app/Http/Controllers/Admin/CampeonatosController.php
+++ app/Http/Controllers/Admin/CampeonatosController.php
@@ -74,9 +74,9 @@ class CampeonatosController extends Controller
 
             $data1 = DateTime::createFromFormat('d/m/Y',$request->input('data_inicio_inscricao'));
 
-            $dados['data_inicio_inscricao'] = Carbon::createFromFormat('d/m/Y', $request->input('data_inicio_inscricao'))->format('Y-m-d');
-            $dados['data_final_inscricao'] = Carbon::createFromFormat('d/m/Y', $request->input('data_final_inscricao'))->format('Y-m-d');
-            $dados['data_campeonato'] = Carbon::createFromFormat('d/m/Y', $request->input('data_campeonato'))->format('Y-m-d');
+            // $dados['data_inicio_inscricao'] = Carbon::createFromFormat('d/m/Y', $request->input('data_inicio_inscricao'))->format('Y-m-d');
+            // $dados['data_final_inscricao'] = Carbon::createFromFormat('d/m/Y', $request->input('data_final_inscricao'))->format('Y-m-d');
+            // $dados['data_campeonato'] = Carbon::createFromFormat('d/m/Y', $request->input('data_campeonato'))->format('Y-m-d');
 
             if (!empty($dados['id'])) 
                 $categoria = Campeonato::updateOrCreate(['id' => $dados['id']], $dados);
