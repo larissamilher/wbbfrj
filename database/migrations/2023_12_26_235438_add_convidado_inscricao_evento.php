73810c7969713e8beab69d39c01c293459f2d134
larissa.silva <larissa.silva@auditto.com.br>
2023-12-26 23:55:14 -0300

    fix

---
+++ database/migrations/2023_12_26_235438_add_convidado_inscricao_evento.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('inscricoes_eventos', function (Blueprint $table) {
+            $table->integer('convidado')->nullable()->after('evento_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
