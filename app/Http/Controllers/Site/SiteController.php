49615fdfd349241ecd1e094759d649e67ca688ad
larissa.silva <larissa.silva@auditto.com.br>
2023-12-29 16:33:47 -0300

    teste

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -40,7 +40,7 @@ class SiteController extends Controller
             'evento' => function ($query) {
                 $query->withTrashed();
             },
-        ])->find(1);
+        ])->find(4);
         
         $conteudo = 'https://wbbfrj.com/eventos/validar/'. str_replace('/', '-', $inscricao->codigo);
         $qrCode = QrCode::size(300)->generate($conteudo);
